6a1f73b4c7a29b5c980dbe64b04fce4f72895e66
gufengbangye <1135523598@qq.com>
2024-11-06 13:46:54 +0800

    feat:新增开发模文件变更会自动重新打包

--- scripts/dev.js
+++ scripts/dev.js
@@ -13,12 +13,17 @@ const require = createRequire(import.meta.url);
 const entry = resolve(__dirname, `../packages/${target}/src/index.ts`);
 const pkg = require(`../packages/${target}/package.json`);
 console.log(pkg);
-esbuild.build({
-  entryPoints: [entry],
-  sourcemap: true,
-  format,
-  bundle: true,
-  platform: "browser",
-  globalName: pkg.buildOptions?.name, //用于如果使用iife格式会将打包的文件变成一个函数 函数名为这个
-  outfile: resolve(__dirname, `../packages/${target}/dist/${target}.js`),
-});
+esbuild
+  .context({
+    entryPoints: [entry],
+    sourcemap: true,
+    format,
+    bundle: true,
+    platform: "browser",
+    globalName: pkg.buildOptions?.name, //用于如果使用iife格式会将打包的文件变成一个函数 函数名为这个
+    outfile: resolve(__dirname, `../packages/${target}/dist/${target}.js`),
+  })
+  .then((ctx) => {
+    console.log(ctx);
+    return ctx.watch();
+  });
